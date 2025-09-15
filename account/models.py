c7f79f5ef938334628a0ff56c0d710589be6c1a4
alieza <alieza.salim021@gmail.com>
2025-09-15 20:10:56 +0330

    make models Profile in account app

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Profile(AbstractUser):
+    mobile = models.CharField(max_length=15, unique=True, verbose_name='Phone Number')
+
+    def __str__(self):
+        return self.get_full_name()
